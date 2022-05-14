90aa7119b2ea63e84025cbbbe1428bf7620a852b
acferM <promacfer@gmail.com>
2022-05-14 16:47:00 -0300

    implemented routes

--- server/src/middlewares/ensureAuthenticated.ts
+++ server/src/middlewares/ensureAuthenticated.ts
@@ -1,34 +1,36 @@
-import { Router } from "express";
+import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
 import { AppError } from "../errors/AppError";
 
-export function EnsureAuthenticated() {
-  return function (target: Router) {
-    target.use((request, response, next) => {
-      const auth = request.headers.authorization
+export function EnsureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const auth = request.headers.authorization
 
-      if (!auth) {
-        throw new AppError('JWT missing', 401)
-      }
+  if (!auth) {
+    throw new AppError('JWT missing', 401)
+  }
 
-      const [, token] = auth.split(' ')
+  const [, token] = auth.split(' ')
 
-      try {
-        const secret = process.env.JWT_SECRET
+  try {
+    const secret = process.env.JWT_SECRET
 
-        const decoded = verify(token, String(secret))
+    const decoded = verify(token, String(secret))
 
-        const { sub } = decoded as { sub: string }
+    const { sub } = decoded as { sub: string }
 
-        request.user = {
-          id: sub,
-        }
+    request.user = {
+      id: sub,
+    }
 
-        return next();
-      } catch {
-        throw new AppError('Invalid JWT', 401)
-      }
-    })
+    console.log('here')
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT', 401)
   }
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,6 +5,9 @@ import { SubmitFeedbackUseCase } from "./use-cases/submit-feedback-use-case";
 import { BcryptHashAdapter } from "./adapters/implementations/hash/bcrypt-hash-adapter";
 import { PrismaUsersRepository } from "./repositories/prisma/prisma-users-repository";
 import { CreateUserUseCase } from "./use-cases/create-user-use-case";
+import { EnsureAuthenticated } from "./middlewares/ensureAuthenticated";
+import { ShowFeedbacksUseCase } from "./use-cases/show-feedbacks-use-case";
+import { AuthenticateUserUseCase } from "./use-cases/authenticate-user-use-case";
 
 const routes = Router()
 
@@ -12,30 +15,44 @@ const nodemailerMailAdapter = new NodemailerMailAdapter()
 const bcryptHashAdapter = new BcryptHashAdapter()
 
 const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-const prismaUsersREpository = new PrismaUsersRepository()
+const prismaUsersRepository = new PrismaUsersRepository()
 
-routes.post('/feedbacks', async (req, res) => {
+routes.post('/feedbacks', EnsureAuthenticated, async (req, res) => {
   const { type, comment, screenshot } = req.body
 
   const submitFeedbackUseCase = new SubmitFeedbackUseCase(
     prismaFeedbacksRepository,
+    prismaUsersRepository,
     nodemailerMailAdapter,
   )
 
   await submitFeedbackUseCase.execute({
     type,
     comment,
+    creatorId: req.user.id,
     screenshot
   })
 
   return res.status(201).send()
 })
 
+routes.get('/feedbacks', EnsureAuthenticated, async (req, res) => {
+  const { page } = req.query
+
+  const showFeedbacks = new ShowFeedbacksUseCase(
+    prismaFeedbacksRepository,
+  )
+
+  const feedbacks = await showFeedbacks.execute(page ? Number(page) : undefined)
+
+  return res.json(feedbacks)
+})
+
 routes.post('/users', async (req, res) => {
   const { name, email, password } = req.body
 
   const createUser = new CreateUserUseCase(
-    prismaUsersREpository,
+    prismaUsersRepository,
     bcryptHashAdapter,
   )
 
@@ -48,6 +65,27 @@ routes.post('/users', async (req, res) => {
   const { password: _, ...userWithoutPassword } = user
 
   return res.status(201).json(userWithoutPassword)
+})
+
+routes.post('/signin', async (req, res) => {
+  const { email, password } = req.body
+
+  const authenticateUser = new AuthenticateUserUseCase(
+    prismaUsersRepository,
+    bcryptHashAdapter,
+  )
+
+  const { token, user } = await authenticateUser.execute({
+    email,
+    password
+  })
+
+  const { password: _, ...userWithoutPassword } = user
+
+  return res.json({
+    token,
+    user: userWithoutPassword,
+  })
 })
 
 export { routes }
